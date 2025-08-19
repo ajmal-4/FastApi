b822450271463a36a5842187c5bc5ff11aee1d41
Mohammed Ajmal P N <mohammed.ajmal@gapblue.com>
2025-08-20 00:35:28 +0530

    Added Categories

--- whatsappBot/app/api/v1/endpoints/menu.py
+++ whatsappBot/app/api/v1/endpoints/menu.py
@@ -4,8 +4,8 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from ....api.deps import get_database
-from ....crud.crud_menu import menu_item
-from ....schemas.menu import MenuItem, MenuItemCreate, MenuItemUpdate
+from ....crud.crud_menu import menu_item, category
+from ....schemas.menu import MenuItem, MenuItemCreate, MenuItemUpdate, Category, CategoryCreate, CategoryUpdate
 
 router = APIRouter()
 
@@ -15,9 +15,9 @@ def get_menu(db: Session = Depends(get_database())):
     return menu_item.get_available_items(db)
 
 @router.get("/category/{category}", response_model=List[MenuItem])
-def get_menu_by_category(category: str, db: Session = Depends(get_database())):
+def get_menu_by_category(category_id: str, db: Session = Depends(get_database())):
     """Get menu items by category"""
-    return menu_item.get_by_category(db, category=category)
+    return menu_item.get_by_category(db, category_id=category_id)
 
 @router.post("/", response_model=MenuItem)
 def create_menu_item(item: MenuItemCreate, db: Session = Depends(get_database())):
@@ -46,4 +46,31 @@ def delete_menu_item(item_id: int, db: Session = Depends(get_database())):
         raise HTTPException(status_code=404, detail="Menu item not found")
     
     updated_item = menu_item.update(db, db_obj=db_item, obj_in={"is_available": False})
+    return updated_item
+
+# Categories
+# ----------------
+
+@router.get("/category", response_model=List[Category])
+def get_all_categories(db: Session = Depends(get_database())):
+    """List all the categories"""
+    return category.get_multi(db)
+
+# Add a new category
+@router.post("/category", response_model=Category)
+def create_category(item: CategoryCreate, db: Session = Depends(get_database())):
+    """Create a new category"""
+    return category.create(db, obj_in=item)
+
+@router.put("/category/{category_id}", response_model=Category)
+def update_category(category_id: int, item: CategoryUpdate, db: Session = Depends(get_database())):
+    """Update an existing category"""
+
+    # Get the existing DB object
+    db_item = category.get(db, id=category_id)
+    if not db_item:
+        raise HTTPException(status_code=404, detail="Menu item not found")
+    
+    # Update the item
+    updated_item = category.update(db, db_obj=db_item, obj_in=item)
     return updated_item
--- whatsappBot/app/crud/crud_menu.py
+++ whatsappBot/app/crud/crud_menu.py
@@ -2,19 +2,24 @@ from typing import List
 from sqlalchemy.orm import Session
 
 from .base import CRUDBase
-from ..models.menu import MenuItem
-from ..schemas.menu import MenuItemCreate, MenuItemUpdate
+from ..models.menu import MenuItem, Category
+from ..schemas.menu import MenuItemCreate, MenuItemUpdate, CategoryCreate, CategoryUpdate
 
 class CRUDMenuItem(CRUDBase[MenuItem, MenuItemCreate, MenuItemUpdate]):
     def get_by_category(self, db: Session, *, category: str) -> List[MenuItem]:
         """ Get menu by category """
         return db.query(MenuItem).filter(
             MenuItem.category == category,
-            MenuItem.is_available == True
+            MenuItem.is_available
         ).all()
     
     def get_available_items(self, db: Session) -> List[MenuItem]:
         """ get all the available items """
-        return db.query(MenuItem).filter(MenuItem.is_available == True).all()
+        return db.query(MenuItem).filter(MenuItem.is_available).all()
 
 menu_item = CRUDMenuItem(MenuItem)
+
+class CRUDCategories(CRUDBase[Category, CategoryCreate, CategoryUpdate]):
+    pass
+
+category = CRUDCategories(Category)
